d9861a65b7621da7624cfad50f12e74ee3c1947e
Stefano Bourscheid <stefano.bourscheid@gmail.com>
2019-04-13 23:36:22 +0200

    Made the draft editor 📖

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:10.15.3-alpine
+
+LABEL "com.github.actions.name"="Release note scribe"
+LABEL "com.github.actions.description"="Generate release notes on Github based on commit msg."
+LABEL "com.github.actions.icon"="circle"
+LABEL "com.github.actions.color"="yellow"
+
+COPY entrypoint.js /entrypoint.js
+
+
+ENTRYPOINT ["node", "/entrypoint.js"]
---
+++ entrypoint.js
@@ -0,0 +1,51 @@
+const {
+  getCurrentBranch,
+  getRepoName,
+  getLastCommitAuthor,
+  getLastCommitMessage,
+} = require('./lib/git');
+const { getLatestReleases, generateDraft, updateReleaseDescription } = require('./lib/github');
+const {
+  formatRepoName,
+  getCurrentVersion,
+  incrementMinorVersion,
+  getLastDraft,
+  validateCommitMessage,
+  formatMessageWithAuthor,
+  generateNewReleaseDescription,
+} = require('./lib/util');
+
+const { releaseBranch, developmentBranch, githubToken } = require('./lib/environment');
+
+(async function entrypoint() {
+  const currentBranch = await getCurrentBranch();
+  if (![releaseBranch, developmentBranch].includes(currentBranch)) {
+    console.log(
+      `⏭ Skipping release note scribe because current branch ${currentBranch} not in the selected branches: ${releaseBranch} or ${developmentBranch}`,
+    );
+    process.exit(78);
+  }
+
+  if (currentBranch === developmentBranch) {
+    // Edit / create draft release
+    const repoName = await formatRepoName(getRepoName());
+    const versionBeingDrafted = await incrementMinorVersion(getCurrentVersion());
+    let lastDraft = await getLastDraft(getLatestReleases());
+    if (!lastDraft) {
+      await generateDraft({ version: versionBeingDrafted, repoName, githubToken });
+      lastDraft = await getLastDraft(getLatestReleases());
+    }
+    const message = await formatMessageWithAuthor(getLastCommitMessage(), getLastCommitAuthor());
+    validateCommitMessage(message, lastDraft.body);
+
+    await updateReleaseDescription({
+      githubToken,
+      version: versionBeingDrafted,
+      releaseNotes: generateNewReleaseDescription(message, lastDraft.body),
+      draftId: lastDraft.id,
+      repoName,
+    });
+  }
+
+  process.exit(0);
+}());
