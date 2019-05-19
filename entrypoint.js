dec6f6316f4dda14b58cdaff05bafcea4d9319d3
Stefano Bourscheid <stefano.bourscheid@gmail.com>
2019-05-19 16:43:20 +0200

    Fixed a few bugs on editing a draft

--- entrypoint.js
+++ entrypoint.js
@@ -12,6 +12,7 @@ const {
 const { releaseBranch, developmentBranch } = require('./lib/environment');
 
 (async function entrypoint() {
+  console.log('🖋 RELEASE NOTE SCRIBE 🖋');
   const currentBranch = await getCurrentBranch();
   if (![releaseBranch, developmentBranch].includes(currentBranch)) {
     console.log(
@@ -26,7 +27,7 @@ const { releaseBranch, developmentBranch } = require('./lib/environment');
       const versionBeingDrafted = incrementMinorVersion(await getCurrentVersion());
       let lastDraft = getLastDraft(await getLatestReleases());
       if (!lastDraft) {
-        await generateDraft(versionBeingDrafted);
+        await generateDraft(versionBeingDrafted, currentBranch);
         lastDraft = getLastDraft(await getLatestReleases());
       }
       const message = formatMessageWithAuthor(
@@ -39,6 +40,7 @@ const { releaseBranch, developmentBranch } = require('./lib/environment');
         version: versionBeingDrafted,
         releaseNotes: generateNewReleaseDescription(message, lastDraft.body),
         draftId: lastDraft.id,
+        branch: currentBranch,
       });
     } catch (e) {
       console.log('🚨 Error on the release note scribe: ', e);
